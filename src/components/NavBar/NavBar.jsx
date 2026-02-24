ace19d1537d501ede3b9318206f6bb5a42b9ecd4
Alyssa Favorito <alyssafavorito2000@gmail.com>
2026-02-24 18:08:42 -0500

    icon update

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,7 +7,7 @@ export default function NavBar(){
         <div className={styles.navbarWrapper}>
             <nav className={`${styles.navbar} navbar navbar-expand-lg`}>
                 <a className={`${styles.navbarBrand} navbar-brand`} href="/">
-                    <img src="3dicons-cube-dynamic-color.png" width="30" height="30" alt="blue cube icon" /> 3D Showcase
+                    <img src="public/3dicons-cube-dynamic-color.png" width="30" height="30" alt="blue cube icon" /> 3D Showcase
                 </a>
                 <button className={`${styles.navbarToggler} navbar-toggler`} type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <RxHamburgerMenu className={styles.hamburger}/>
