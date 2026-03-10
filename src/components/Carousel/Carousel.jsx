e6cbd3a6596cf6719298e5e16ea985b5621b6cc7
Alyssa Favorito <alyssafavorito2000@gmail.com>
2026-03-10 08:58:50 -0400

    Carousel and Footer

---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,32 @@
+import Car from "../Car/Car";
+
+export default function Carousel(){
+    return (
+        <div id="carouselExampleIndicators" data-ride="carousel" className="carousel-indicators">
+            <ol className="carouselIndicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div className="carousel-inner">
+                <div className="active carousel-item">
+                    <Car />
+                </div>
+                <div className="carousel-item">
+                    <Car />
+                </div>
+                <div className="carousel-item">
+                    <Car />
+                </div>
+            </div>
+            <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="sr-only">Previous</span> 
+            </a>
+            <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="sr-only">Next</span>
+            </a>
+        </div>
+    )
+}
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from "Footer.module.scss";
+
+export default function Footer() {
+    return (
+        <div className={styles.footerOuter}>
+            <div className={styles.footerInner}>
+                <p>Favorito © 2026</p>
+            </div>
+        </div>
+    )
+}
