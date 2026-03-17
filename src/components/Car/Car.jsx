0f407808bf2d7be76023ae9c7175676363531570
Alyssa Favorito <alyssafavorito2000@gmail.com>
2026-03-17 14:41:01 -0400

    editing car component

--- src/components/Car/Car.jsx
+++ src/components/Car/Car.jsx
@@ -2,7 +2,7 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { useLoader } from "@react-three/fiber";
 import { useFrame } from "@react-three/fiber";
 
-export default function Car({ url = "/E24_Materials.gltf"}) {
+export default function Car({ url = "/E24_Materials.gltf" }) {
     const gltf = useLoader(GLTFLoader, url);
      useFrame(({ clock }) => {
         const turn = clock.getElapsedTime()/2;
