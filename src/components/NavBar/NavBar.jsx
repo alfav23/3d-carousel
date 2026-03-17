f9c725d178819421ab93dd743b98b39008273b98
Alyssa Favorito <alyssafavorito2000@gmail.com>
2026-03-17 12:13:46 -0400

    changed font and added cursor blinking animation; changed navbar to be custom 3d icon

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,9 +7,12 @@ export default function NavBar(){
         <div className={styles.navbarWrapper}>
             <nav className={`${styles.navbar} navbar navbar-expand-lg`}>
                 <a className={`${styles.navbarBrand} navbar-brand`} href="/">
-                    <img src="/3dicons-cube-dynamic-color.png" width="30" height="30" alt="blue cube icon" /> 3D Showcase
+                    <img src="/e24icon.png" width="120" height="100" alt="e24 3d icon" /> 
+                    <span className={styles.cursor}>
+                        choose your vehicle<span>|</span>
+                    </span>
                 </a>
-                <button className={`${styles.navbarToggler} navbar-toggler`} type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                {/* <button className={`${styles.navbarToggler} navbar-toggler`} type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <RxHamburgerMenu className={styles.hamburger}/>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
@@ -17,7 +20,7 @@ export default function NavBar(){
                         <a className={`${styles.navItem} nav-item nav-link active`} href="/">Home</a>
                         <a className={`${styles.navItem} nav-item nav-link`} href="/playground">Playground</a>
                     </div>
-                </div>
+                </div> */}
             </nav>
         </div>
     );
